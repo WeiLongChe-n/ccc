deb4a1f89ef6e3862035f144910c704a0c1841a6
陈威隆 <18700880154@163.com>
2024-07-25 18:00:48 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: 陈威隆 18700880154@163.com
+ * @Date: 2024-07-25 14:32:26
+ * @LastEditors: 陈威隆 18700880154@163.com
+ * @LastEditTime: 2024-07-25 17:57:49
+ * @FilePath: \home-qiankun\vite.config.ts
+ * @Description: 
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:'/WeiLongChe-n/ccc',
+  plugins: [vue()],
+})
